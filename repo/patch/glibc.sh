6656d52d9ffc297a231967cdef81bb3651c0fee8
hourin <phucjaki@gmail.com>
2026-01-20 22:01:28 +0700

    Added: LFS glibc patch

---
+++ repo/patch/glibc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+NAME=glibc
+VERSION=2.42
+URL=" https://www.linuxfromscratch.org/patches/lfs/12.4/glibc-2.42-fhs-1.patch"
+
+unpack() {
+    :
+}
+
+pkg_build() {
+    :
+}
+
+pkg_install() {
+    mkdir -pv patches
+    mv -v glibc-2.42-fhs-1.patch patches/
+}
+
