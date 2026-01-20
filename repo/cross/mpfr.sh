987bbd00ff310af9c1fda4eacba744eb04cf5110
hourin <phucjaki@gmail.com>
2026-01-20 17:33:13 +0700

    Added: mpfr script

---
+++ repo/cross/mpfr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+NAME=mpfr
+VERSION=4.2.2
+URL="https://ftp.gnu.org/gnu/mpfr/mpfr-$VERSION.tar.xz"
+
+unpack() {
+    tar xf "$NAME-$VERSION.tar.xz"
+}
+
+pkg_build() {
+    :
+}
+
+pkg_install() {
+    :
+}
+
