13dbc562d5f0040312b4d7b968a98cfd7bdb38bb
hourin <phucjaki@gmail.com>
2026-01-21 17:46:56 +0700

    Added: create-full-fs helper script

---
+++ create-full-fs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+read -p "Did you verify that you're in a chroot LFS environment? (y)Yes/n(No): " choice
+case $choice in
+    [yY]* ) echo "OK" ;;
+    [nN]* ) exit 1 ;;
+    * ) exit 1 ;;
+esac
+
+mkdir -pv /{boot,home,mnt,opt,srv}
+
+mkdir -pv /etc/{opt,sysconfig}
+mkdir -pv /lib/firmware
+mkdir -pv /media/{floppy,cdrom}
+mkdir -pv /usr/{,local/}{include,src}
+mkdir -pv /usr/lib/locale
+mkdir -pv /usr/local/{bin,lib,sbin}
+mkdir -pv /usr/local/games
+mkdir -pv /usr/share/games
+mkdir -pv /usr/{,local/}share/{color,dict,doc,info,locale,man}
+mkdir -pv /usr/{,local/}share/{misc,terminfo,zoneinfo}
+mkdir -pv /usr/{,local/}share/man/man{1..8}
+mkdir -pv /var/{cache,local,log,mail,opt,spool}
+mkdir -pv /var/lib/{color,misc,locate}
+
+ln -sfv /run /var/run
+ln -sfv /run/lock /var/lock
+
+install -dv -m 0750 /root
+install -dv -m 1777 /tmp /var/tmp
+
