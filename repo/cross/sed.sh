a4845fc5c09fc7d9ef47c4b3339766b3d46d350c
hourin <phucjaki@gmail.com>
2026-01-21 11:55:22 +0700

    Added: cross/sed

---
+++ repo/cross/sed.sh
@@ -0,0 +1,25 @@
+NAME=sed
+VERSION=4.9
+URL="https://ftp.gnu.org/gnu/$NAME/$NAME-$VERSION.tar.xz"
+
+unpack() {
+    tar xf "$NAME-$VERSION.tar.xz"
+}
+
+pkg_build() {
+    cd $NAME-$VERSION
+
+    mkdir -pv build && cd build
+    ../configure \
+        --prefix=/usr \
+        --host=$LFS_TGT \
+        --build=$(../build-aux/config.guess)
+
+    make
+}
+
+pkg_install() {
+    cd "$NAME-$VERSION/build"
+    make DESTDIR=$LFS install
+}
+
